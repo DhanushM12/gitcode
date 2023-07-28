de5f988e024953ce118ee51a64f8ce34eab9e606
DhanushM12 <dhanushmukhi@gmail.com>
2023-07-28 21:52:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// localStorage.setItem('batchName', 'jan23')
+// console.log(localStorage.getItem('batchName'));
+// localStorage.setItem('abx', 'kndkndk');
+// localStorage.removeItem('abx');
+
+// sessionStorage.setItem('newbatch', 'jan2023');
+// console.log(sessionStorage.getItem('newbatch'))
+// sessionStorage.setItem('abx', 'kndkndk');
+// sessionStorage.removeItem('abx')
+
+// document.cookie = 'mynewbatch=jan23; Expires=Fri Jul 28 2023 21:34:50 GMT+0530'
+// console.log(document.cookie)
